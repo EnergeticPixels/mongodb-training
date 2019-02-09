f714a370f97f4b362b2d6ff3c198d71d39663330
Anthony Jackman <energeticpixels@gmail.com>
2019-02-08 19:07:23 -0600

    fixes increment mongo op

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const UserSchema = new Schema({
     },
     required: [true, 'Name is required.']
   },
+  likes: Number,
   posts: [PostSchema]
 });
 
--- test/user.js
+++ test/user.js
@@ -123,16 +123,16 @@ describe('User Model', () => {
     let joe;
 
     beforeEach((done) => {
-      joe = new User({ name: 'Joe', postCount: 0 });
+      joe = new User({ name: 'Joe', likes: 0 });
       joe.save()
         .then(() => done());
     });
 
-    xit('a user postCount increments by 1', (done) => {
-      User.updateMany({ name: 'Joe' }, { $inc:{ postCount: 1 }})
+    it('a user postCount increments by 1', (done) => {
+      User.updateMany({ name: 'Joe' }, { $inc:{ likes: 1 }})
         .then(() => User.findOne({ name: 'Joe' }))
         .then((user) => {
-          assert(user.postCount === 1);
+          assert(user.likes === 1);
           done();
         });
     });
