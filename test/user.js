73fcd0333a20c653c0040c998208a5802a451f5c
Anthony Jackman <energeticpixels@gmail.com>
2019-02-12 21:35:41 -0600

    adds skip/limit/sort to result set; git email

--- test/user.js
+++ test/user.js
@@ -326,4 +326,28 @@ describe('User Model', () => {
     });
   });
 
+  describe('Query Modifiers', () => {
+    let joe, alex, maria, zach;
+    beforeEach((done) => {
+      joe = new User({ name: 'Joe'});
+      alex = new User({ name: 'Alex'});
+      maria = new User({ name: 'Maria'});
+      zach = new User({ name: 'Zach'});
+
+      Promise.all([ joe.save(), alex.save(), maria.save(), zach.save() ])
+        .then(() => done())
+    });
+
+    it('can skip and limit the result set', (done) => {
+      User.find({}).sort({ name: 1}).skip(1).limit(2)
+        .then((users) => {
+          assert(users.length === 2);
+          assert(users[0].name === 'Joe');
+          assert(users[1].name === 'Maria');
+          done();
+        })
+
+    });
+  })
+
 })
